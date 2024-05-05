f93cb1ae4543e05ad51079fb4935e03b8bba0e8c
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2024-05-05 17:41:10 +0100

    block the A button entirely and add a command to explain the mod

--- noabutton.lua
+++ noabutton.lua
@@ -1,10 +1,18 @@
-function resetAButton(m)
-	if m.action == ACT_JUMP or m.action == ACT_JUMP_KICK then
-		if m.prevAction == ACT_JUMP or m.prevAction == ACT_JUMP_KICK then
-			m.action = ACT_IDLE
-		else
-			m.action = m.prevAction
-		end
-	end
+-- name: No A Button Challenge
+-- description: Disables the A Button
+
+function disableAButton(m)
+	m.controller.buttonDown = m.controller.buttonDown & ~A_BUTTON
+    m.controller.buttonPressed = m.controller.buttonPressed & ~A_BUTTON
 end
-hook_event(HOOK_BEFORE_MARIO_UPDATE, resetAButton)
+
+function displayInfo(m)
+	djui_chat_message_create('No A Button Challenge:')
+	djui_chat_message_create('Your A Button is disabled')
+	djui_chat_message_create('The goal is to collect as many stars and hopefully beat the game')
+	djui_chat_message_create('Use team work to overcome this limitation, Good luck!')
+	return true
+
+end
+hook_event(HOOK_BEFORE_MARIO_UPDATE, disableAButton)
+hook_chat_command('a', "Show Info about the No A Button Challenge", displayInfo)
